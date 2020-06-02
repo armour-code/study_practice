6d02a7a0b723d2ec6ba056029574137df3f54ac9
armour-code <62875480+armour-code@users.noreply.github.com>
2020-06-02 09:51:38 +0800

    好看的分形数
    提供递归的启发

---
+++ 分形树.py
@@ -0,0 +1,31 @@
+# author: fzb
+# time: 2020/3/30
+# function:  分形树
+from turtle import *
+Draw = Turtle()
+MyDraw = Draw.getscreen()
+
+
+def DrawTree(DrawPin, PinLine, Linesize):
+    if PinLine > 5:
+        if Linesize > 1:
+            DrawPin.pensize(Linesize)
+        DrawPin.pencolor("brown")
+        if PinLine < 20:
+            DrawPin.pencolor("green")
+        DrawPin.forward(PinLine)
+        DrawPin.left(30)
+        DrawTree(DrawPin, PinLine-15, Linesize-1)
+        DrawPin.right(60)
+        DrawTree(DrawPin, PinLine-15, Linesize-1)
+        DrawPin.left(30)
+        DrawPin.pencolor("brown")
+        if PinLine < 34:
+            DrawPin.pencolor("green")
+        DrawPin.backward(PinLine)
+
+
+Draw.left(90)
+Draw.sety(-50)
+DrawTree(Draw, 100, 6)
+MyDraw.exitonclick()
